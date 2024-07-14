a5318024858961fd3bdaaf4e8e4c461ebd8e2346
Ran Isenberg <ran.isenberg@ranthebuilder.cloud>
2024-07-14 08:40:26 +0300

    fix test

--- tests/infrastructure/test_cdk.py
+++ tests/infrastructure/test_cdk.py
@@ -13,5 +13,5 @@ def test_synthesizes_properly():
     template = Template.from_stack(service_stack)
 
     template.resource_count_is('AWS::DynamoDB::GlobalTable', 1)  # main db
-    template.resource_count_is('AWS::ServiceCatalog::CloudFormationProduct', 2)  # two products
+    template.resource_count_is('AWS::ServiceCatalog::CloudFormationProduct', 3)  # two products
     template.resource_count_is('AWS::ServiceCatalog::Portfolio', 1)  # one portfolio
